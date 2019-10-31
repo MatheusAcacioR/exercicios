6be1179041cd8073baf53d95c93b46dc69764ecf
matheus-2000 <55986464+matheus-2000@users.noreply.github.com>
2019-10-31 13:57:18 -0300

    Add files via upload

---
+++ aula7.m
@@ -0,0 +1,40 @@
+﻿close all;           #RESETA SISTEMA
+clear all; 
+
+ [a,map] = imread('pcb1bin.png');         #LER IMAGEM
+ a = a*255;
+ figure
+ imshow(a);
+ pause
+
+ b = imfill(a,'holes'); %fillHoles            #PREENCHER BURACOS
+ figure
+ imshow(b);
+ pause
+ 
+ c = imabsdiff(a,b); % Sub a-b           #DIFERENÇA A-B
+ figure
+ imshow(c)
+ %pause
+ 
+ SE = strel('square', 16);  #ABERTURA 
+ d = imopen(b, SE);
+ figure
+ imshow(d);
+ %pause;
+ 
+ e = bitand(d, a);   #BIT DE RESULTADO QUANDO AMBOS FOREM VERDADEIROS
+ figure
+ imshow(e);
+ pause
+ 
+ SE = strel('rectangle',[8 25]);  #ABERTURA 
+ f = imopen(b, SE);
+ figure
+ imshow(f);
+ pause;
+ 
+ g = bitand(f, a);   #BIT DE RESULTADO QUANDO AMBOS FOREM VERDADEIROS
+ figure
+ imshow(g);
+ pause
---
+++ my_script.m
@@ -0,0 +1,25 @@
+﻿close all
+clear all
+
+I = imread('photo 1.tif');
+imshow(I);
+level = graythresh(I);
+
+BW = im2bw(I,level);
+figure
+imshow(BW)
+
+se = strel('disk',5,0);
+Ierode = imerode(BW,se); 
+figure
+imshow(Ierode); 
+
+%Idilate = imdilate(BW,se);
+%figure
+%imshow(Idilate); 
+
+Iopen = imdilate(Ierode,se);
+figure
+imshow(Iopen);
+
+
